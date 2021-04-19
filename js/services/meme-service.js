264d81c76a4ef57a043d3c42723150a6e40e38c3
Amir Liberzon <amir.lib@gmail.com>
2021-04-19 12:33:40 +0300

    styled gallery component

---
+++ js/services/meme-service.js
@@ -0,0 +1,9 @@
+var gMeme = createDefaultMeme();
+
+function createDefaultMeme() {
+  return {
+    lines: [],
+    selectedImgId: 0,
+    selectedLineId: 0,
+  }
+}
---
+++ js/services/storage-service.js
@@ -0,0 +1,11 @@
+function loadFromStorage(key) {
+  const str = localStorage.getItem(key)
+
+  return JSON.parse(str);
+}
+
+function saveToStorage(key, val) {
+  const str = JSON.stringify(val);
+
+  localStorage.setItem(key, str);
+}
